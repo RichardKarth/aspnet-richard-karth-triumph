289f97a13eaa275cd757490db48f481c7a115835
Richard Karth <richard.k.triumph@gmail.com>
2026-04-29 12:36:03 +0200

    Added controllers and a my account page with working buttons for menu

---
+++ Presentation.WebApp/Models/BecomeMemberModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Presentation.WebApp.Models
+{
+    public class BecomeMemberModel
+    {
+        public string Email { get; set; } = null!;
+    }
+}
--- Presentation.WebApp/Models/SignInViewModel.cs
+++ Presentation.WebApp/Models/SignInViewModel.cs
@@ -2,4 +2,7 @@
 
 public class SignInViewModel
 {
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public bool RememberMe { get; set; }
 }
